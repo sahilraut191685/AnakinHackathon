996203864abc052e1d72b8dc35940c13afa0a61b
sahilraut191685 <sahilraut200619@gmail.com>
2026-09-10 19:01:58 +0530

    spacing removed

--- deceive-agent/server/index.js
+++ deceive-agent/server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config();
+
 const { searchWeb } = require("./services/searchService");
 const { askAgent } = require("./services/llmService");
 const { scrapeWebsite } = require("./services/scraperService");
